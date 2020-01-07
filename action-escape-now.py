12acd9e55151232a813c5f6d67f038994ce07620
grebemal <malte.grebe@gmx.de>
2020-01-07 10:16:17 +0100

    added rest of the words

--- action-escape-now.py
+++ action-escape-now.py
@@ -21,23 +21,27 @@ def subscribe_intent_callback(hermes, intent_message):
     intentname = intent_message.intent.intent_name
 
     if intentname == user_intent("takeYourMeds"):
-        name = random.choice(NAMES)
         result_sentence = "Das kann ich nicht"
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Kaleidoskop"):
-        result_sentence = "Könnt ihr das auch sehen?"
+    
+    elif intentname == user_intent("Adler"):
+        result_sentence = NAMES[11]
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Explosion"):
-        result_sentence = "Tick! Tack! Tick! Tack!"
+    
+    elif intentname == user_intent("Apfel"):
+        result_sentence = NAMES[9]
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Gewicht"):
-        result_sentence = "Das ist mir zu schwer!"
+    
+    elif intentname == user_intent("Becken"):
+        result_sentence = NAMES[0]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+    
+    elif intentname == user_intent("Zwerg"):
+        result_sentence = NAMES[5]
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
 
@@ -46,8 +50,38 @@ def subscribe_intent_callback(hermes, intent_message):
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
 
-    elif intentname == user_intent("Becken"):
-        result_sentence = NAMES[0]
+    elif intentname == user_intent("Elch"):
+        result_sentence = NAMES[13]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)    
+
+    elif intentname == user_intent("Explosion"):
+        result_sentence = "Tick! Tack! Tick! Tack!"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Koch"):
+        result_sentence = NAMES[7]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Hase"):
+        result_sentence = NAMES[16]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+    
+    elif intentname == user_intent("Hasenkampf"):
+        result_sentence = NAMES[20]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Smiley"):
+        result_sentence = "Könnt ihr das auch sehen?"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Kolibri"):
+        result_sentence = NAMES[21]
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
 
@@ -56,8 +90,98 @@ def subscribe_intent_callback(hermes, intent_message):
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
 
+    elif intentname == user_intent("Nichts"):
+        result_sentence = "Könnt ihr das auch sehen?"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Pfau"):
+        result_sentence = NAMES[15]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Hexe"):
+        result_sentence = "Könnt ihr das auch sehen?"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Frau"):
+        result_sentence = "Könnt ihr das auch sehen?"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Schaedel"):
+        result_sentence = "Könnt ihr das auch sehen?"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Reproduktionssystem"):
+        result_sentence = NAMES[11]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Schwellkoerper"):
+        result_sentence = "Wir haben keine Zeit für Witze"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Baum"):
+        result_sentence = NAMES[8]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Teufel"):
+        result_sentence = "Könnt ihr das auch sehen?"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Vampir"):
+        result_sentence = "Aber nicht die, die im Sonnenlicht glitzern."
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Panda"):
+        result_sentence = NAMES[6]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
     elif intentname == user_intent("Murmeltier"):
         result_sentence = NAMES[1]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Raumschiff"):
+        result_sentence = NAMES[19]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Lunge"):
+        result_sentence = NAMES[17]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("General"):
+        result_sentence = NAMES[2]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Gewicht"):
+        result_sentence = "Das ist mir zu schwer!"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Nikolaus"):
+        result_sentence = NAMES[14]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence) 
+
+    elif intentname == user_intent("Heuschrecke"):
+        result_sentence = NAMES[10]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Kaleidoskop"):
+        result_sentence = "Könnt ihr das auch sehen?"
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
 
@@ -66,8 +190,33 @@ def subscribe_intent_callback(hermes, intent_message):
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
 
-    elif intentname == user_intent("General"):
-        result_sentence = NAMES[2]
+    elif intentname == user_intent("Cowboy"):
+        result_sentence = "Yee! Haa!"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Mamma"):
+        result_sentence = NAMES[18]
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Tuersteher"):
+        result_sentence = "Könnt ihr das auch sehen?"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Rueckenschmerzen"):
+        result_sentence = "Könnt ihr das auch sehen?"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Krone"):
+        result_sentence = "Könnt ihr das auch sehen?"
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+
+    elif intentname == user_intent("Watchmen"):
+        result_sentence = "Könnt ihr das auch sehen?"
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
 
@@ -88,106 +237,6 @@ def subscribe_intent_callback(hermes, intent_message):
 
     elif intentname == user_intent("HeptapodischVier"):
         result_sentence = NAMES[4]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Kolibri"):
-        result_sentence = NAMES[21]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Zwerg"):
-        result_sentence = NAMES[5]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Hasenkampf"):
-        result_sentence = NAMES[20]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Panda"):
-        result_sentence = NAMES[6]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Raumschiff"):
-        result_sentence = NAMES[19]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-        
-    elif intentname == user_intent("Koch"):
-        result_sentence = NAMES[7]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Mamma"):
-        result_sentence = NAMES[18]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Baum"):
-        result_sentence = NAMES[8]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Lunge"):
-        result_sentence = NAMES[17]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Apfel"):
-        result_sentence = NAMES[9]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence) 
-
-    elif intentname == user_intent("Hase"):
-        result_sentence = NAMES[16]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Heuschrecke"):
-        result_sentence = NAMES[10]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Pfau"):
-        result_sentence = NAMES[15]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Adler"):
-        result_sentence = NAMES[11]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)   
-
-    elif intentname == user_intent("Nikolaus"):
-        result_sentence = NAMES[14]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence) 
-
-    elif intentname == user_intent("Elch"):
-        result_sentence = NAMES[13]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)                
-
-    elif intentname == user_intent("Reproduktionssystem"):
-        result_sentence = NAMES[11]
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Schwellkoerper"):
-        result_sentence = "Wir haben keine Zeit für Witze"
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Vampir"):
-        result_sentence = "Aber nicht die, die im Sonnenlicht glitzern."
-        current_session_id = intent_message.session_id
-        hermes.publish_end_session(current_session_id, result_sentence)
-
-    elif intentname == user_intent("Cowboy"):
-        result_sentence = "Yee! Haa!"
         current_session_id = intent_message.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
 
